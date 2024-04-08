c2a3e3d2432343fcd77d56817ecb4d5f28ad95c9
lawrence-254 <lodhiambo26@gmail.com>
2024-04-08 15:34:01 +0300

    dependenciesInstall

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const cors = require('cors');
 // const passport = require('passport');
 // const path = require('path');
+const dbConnection = require('./configs/dbConfig');
+//======================================================================================================
+//routes
 const userRoutes = require('./routes/userRoutes');
 const podcastRoutes = require('./routes/podcastRoutes');
 const episodeRoutes = require('./routes/episodeRoutes');
@@ -11,16 +14,28 @@ const commentRoutes = require('./routes/commentRoutes');
 const likeRoutes = require('./routes/likeRoutes');
 const subscriptionRoutes = require('./routes/subscriptionRoutes');
 const playbackRoutes = require('./routes/playbackRoutes');
+//======================================================================================================
 
+// Load environment variables
+
+
+
+require('dotenv').config();
+dbConnection();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+}));
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(passport.initialize());
 // app.use(passport.session());
 
 // require('./config/passport')(passport);
+//======================================================================================================
 
 app.use('/api/user', userRoutes);
 app.use('/api/podcast', podcastRoutes);
@@ -30,7 +45,7 @@ app.use('/api/like', likeRoutes);
 app.use('/api/subscription', subscriptionRoutes);
 app.use('/api/playback', playbackRoutes);
 
-
+//=======================================================================================================
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
