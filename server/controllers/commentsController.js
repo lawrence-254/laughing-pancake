e4d4cb5eb5186eabaa8adce7aa76703067541fd1
lawrence-254 <lodhiambo26@gmail.com>
2024-04-09 13:18:04 +0300

    comments

--- server/controllers/commentsController.js
+++ server/controllers/commentsController.js
@@ -19,3 +19,59 @@ const addComment = asyncHandler(async (req, res, next) => {
     }
 }
 );
+
+const getAllComments = asyncHandler(async (req, res, next) => {
+    try {
+        const comments = await Comments.find().populate('user', 'username');
+        res.status(200).json({ success: true, data: comments });
+    } catch (error) {
+        next(error);
+    }
+}
+);
+
+const getComment = asyncHandler(async (req, res, next) => {
+    try {
+        const comment = await Comments.findById(req.params.id).populate('user', 'username');
+        if (!comment) {
+            return next(new ErrorResponse('Comment not found', 404));
+        }
+        res.status(200).json({ success: true, data: comment });
+    } catch (error) {
+        next(error);
+    }
+}
+);
+
+const updateComment = asyncHandler(async (req, res, next) => {
+    try {
+        const comment = await Comments.findById(req.params.id);
+        if (!comment) {
+            return next(new ErrorResponse('Comment not found', 404));
+        }
+        const { text } = req.body;
+        comment.commentText = text || comment.commentText;
+
+        await comment.save();
+        res.status(200).json({ success: true, data: comment });
+    } catch (error) {
+        next(error);
+    }
+}
+);
+
+const deleteComment = asyncHandler(async (req, res, next) => {
+    try {
+        const comment = await Comments.findById(req.params.id);
+        if (!comment) {
+            return next(new ErrorResponse('Comment not found', 404));
+        }
+        await comment.remove();
+        res.status(200).json({ success: true, data: {} });
+    } catch (error) {
+        next(error);
+    }
+}
+);
+
+module.exports = { addComment, getAllComments, getComment, updateComment, deleteComment };
