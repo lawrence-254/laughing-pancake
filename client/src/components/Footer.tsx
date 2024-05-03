a0969f0f33152891c4d3870c852abdc838017c3b
lawrence-254 <lodhiambo26@gmail.com>
2024-05-03 13:45:32 +0300

    layout

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+const FooterContainer = styled.footer`
+width: 100%;
+display:  flex;
+flex-direction: column;
+align-items:center;
+position: absolute;
+bottom:0;
+`;
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+        <div className="">
+            <h1>Train your app with lobe</h1>
+        </div>
+        <hr />
+        <div className=""></div>
+    </FooterContainer>
+  )
+}
+
+export default Footer
---
+++ client/src/pages/MyPodcasts.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyPodcasts = () => {
+  return (
+    <div>MyPodcasts</div>
+  )
+}
+
+export default MyPodcasts
