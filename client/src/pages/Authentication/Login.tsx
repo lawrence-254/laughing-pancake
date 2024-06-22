65bbb32056e93a7f3b73c01efc7b1682122304ca
lawrence-254 <lodhiambo26@gmail.com>
2024-06-22 15:32:21 +0300

    formSetup

--- client/src/pages/Authentication/Login.tsx
+++ client/src/pages/Authentication/Login.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { yupResolver } from '@hookform/resolvers/yup'
+import {useForm, submitHandler} from 'react-hook-form'
+import * as yup from 'yup'
 
+interface IFormInput {
+  email: string
+  password: string
+}
 const Login = () => {
+  const schema = yup.object().shape({
+    email: yup.string().email().required("Email is required"),
+    password: yup.string().required("Password is required")
+  })
+  const {login, handleSubmit, formState:{errors},}=useForm<IFormInput>({
+    resolver:yupResolver(schema)
+  })
+  const handleSubmit =()=>{
+    console.log('ghj');
+
+  }
+
+
   return (
     <div>
-      
+      <h1>LOGIN TO YOUR ACCOUNT</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input type="email" />
+        </div>        <div>
+          <label>Password</label>
+          <input type="password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   )
 }
