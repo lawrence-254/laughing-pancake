399e330cbfd4617cfa90516f6b91795b2de8a25b
lawrence-254 <lodhiambo26@gmail.com>
2024-06-22 12:06:50 +0300

    setuproutes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,21 @@
 
-import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Authentication/Login'
+import Signup from './pages/Authentication/Signup'
+
 
 function App() {
 
   return (
-    <>
-    <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+      </Routes>
+    </Router>
   )
 }
 
