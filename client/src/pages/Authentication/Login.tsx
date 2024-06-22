7d56a9959b600ceaba92b44ba721a1d09bb6087b
lawrence-254 <lodhiambo26@gmail.com>
2024-06-22 17:34:58 +0300

    apiSetup

--- client/src/pages/Authentication/Login.tsx
+++ client/src/pages/Authentication/Login.tsx
@@ -1,40 +1,45 @@
-import { yupResolver } from '@hookform/resolvers/yup'
-import {useForm, submitHandler} from 'react-hook-form'
-import * as yup from 'yup'
+import React from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import * as yup from 'yup';
 
 interface IFormInput {
-  email: string
-  password: string
+  email: string;
+  password: string;
 }
-const Login = () => {
+
+const Login: React.FC = () => {
   const schema = yup.object().shape({
     email: yup.string().email().required("Email is required"),
     password: yup.string().required("Password is required")
-  })
-  const {login, handleSubmit, formState:{errors},}=useForm<IFormInput>({
-    resolver:yupResolver(schema)
-  })
-  const handleSubmit =()=>{
-    console.log('ghj');
+  });
 
-  }
+  const { register, handleSubmit, formState: { errors } } = useForm<IFormInput>({
+    resolver: yupResolver(schema)
+  });
 
+  const onSubmit: SubmitHandler<IFormInput> = data => {
+    console.log(data);
+  };
 
   return (
     <div>
       <h1>LOGIN TO YOUR ACCOUNT</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label>Email:</label>
-          <input type="email" />
-        </div>        <div>
-          <label>Password</label>
-          <input type="password" />
+          <input type="email" {...register("email")} />
+          {errors.email && <p>{errors.email.message}</p>}
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" {...register("password")} />
+          {errors.password && <p>{errors.password.message}</p>}
         </div>
         <button type="submit">Login</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
